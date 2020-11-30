2d7bc74485947a64918e1aab3d15c6855ec9d370
bignian <114035931+bignian@users.noreply.github.com>
2020-11-30 11:47:02 +0000

    update requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+    if curren
