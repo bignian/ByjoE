ec087ec49afad8a58a378414ba2a51d63df7f8ce
bignian <114035931+bignian@users.noreply.github.com>
2020-12-13 19:04:25 +0000

    Version 0.0.2.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,4 +31,5 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post w
+        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.json { render :show, status: :created, loc
