10e357d329528365d22712386053936874b5692a
bignian <114035931+bignian@users.noreply.github.com>
2020-12-15 06:01:57 +0000

    Debugging JavaScript code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,4 +32,6 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, loc
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unpro
