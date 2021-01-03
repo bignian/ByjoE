d85633e581297c7b86e53b566b971c075467b8ed
bignian <114035931+bignian@users.noreply.github.com>
2021-01-03 19:08:12 +0000

    working copy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,4 +45,6 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-       
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html 
