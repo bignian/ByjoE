667544a57630db4b1eb602aac28a039401ef4204
bignian <114035931+bignian@users.noreply.github.com>
2021-02-19 14:39:24 +0000

    Update apps.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,4 +66,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through
