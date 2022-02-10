3586124e694720e317997e70de088645f2d75aae
bignian <114035931+bignian@users.noreply.github.com>
2022-02-10 18:09:26 +0000

    prev

--- config/application.rb
+++ config/application.rb
@@ -3,4 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limi
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TradingAp
