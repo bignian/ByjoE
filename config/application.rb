be8f00cf98d2b48dfa09ba9848bf56f4b37ba5db
bignian <114035931+bignian@users.noreply.github.com>
2022-02-15 22:23:46 +0000

    New class.

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,5 @@ module TradingApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.ro
