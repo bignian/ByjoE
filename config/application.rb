144152177bed136c5c6f850360dac06b4d7960b3
bignian <114035931+bignian@users.noreply.github.com>
2022-02-15 10:14:16 +0000

    :confetti_ball: Added .gitattributes & .gitignore files

--- config/application.rb
+++ config/application.rb
@@ -13,4 +13,7 @@ module TradingApp
 
     # Configuration for the application, engines, and railties goes here.
     #
-    # These settings can be overridden in specific environments using t
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    #
