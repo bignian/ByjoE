aef99bf6ab773d103e32854128f3d1d310f96d3c
bignian <114035931+bignian@users.noreply.github.com>
2022-02-18 14:38:26 +0000

    [ENGOPS-540] shared file update from distribute.groovy

--- config/application.rb
+++ config/application.rb
@@ -17,4 +17,6 @@ module TradingApp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.ro
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
