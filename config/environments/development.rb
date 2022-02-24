b038eae1148292af30f12bca7e5960b9d54975bd
bignian <114035931+bignian@users.noreply.github.com>
2022-02-24 18:17:27 +0000

    Update development.md

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence ove
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your applic
