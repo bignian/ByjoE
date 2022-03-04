ce4f56cd292ae29294b6786aca56015aab1ca2a6
bignian <114035931+bignian@users.noreply.github.com>
2022-03-04 06:13:05 +0000

    Info update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perfor
