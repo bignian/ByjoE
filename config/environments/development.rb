41ae2deba3e1bab7143feedd38db966d4f17a369
bignian <114035931+bignian@users.noreply.github.com>
2022-03-11 21:17:13 +0000

    Regenerate gemspec for version 0.5.5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.y
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
+  # Don't care if the mailer ca
