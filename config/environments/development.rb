40a40ed8c435020477893b99d39d695a1073fda1
bignian <114035931+bignian@users.noreply.github.com>
2022-03-18 13:09:04 +0000

    Update release version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise exceptions for disallowed dep
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which 
