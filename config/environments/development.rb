4e0b5c3b7eea88549f4cadf63ac3214097da2a26
bignian <114035931+bignian@users.noreply.github.com>
2022-03-19 23:01:36 +0000

    update docs [skip ci]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation = :raise
 
   # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_wa
+  config.active_support.disallowed_deprecation_warnings = []
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_recor
