abad5c4322678783df02612cde652de6b91f0c6a
bignian <114035931+bignian@users.noreply.github.com>
2022-03-20 20:25:11 +0000

    Some progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_recor
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  con
