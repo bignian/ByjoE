b1be8b87844d177733da246085ef86b0d899df01
bignian <114035931+bignian@users.noreply.github.com>
2022-03-22 08:29:12 +0000

    Update BigQuery-table-search-data.rst

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing transla
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rend
