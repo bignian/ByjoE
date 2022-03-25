a32c5a4f3a2f77a750cc6dd4333bdce57ea28a45
bignian <114035931+bignian@users.noreply.github.com>
2022-03-25 11:35:15 +0000

    f2fs: check upper bound of ino value in f2fs_nfs_get_inode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.perform_de
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action
