26fcfdd60b42fdc208b69cdd4ee740f411a20451
bignian <114035931+bignian@users.noreply.github.com>
2022-03-29 15:34:17 +0000

    updated filter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence ove
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  confi
