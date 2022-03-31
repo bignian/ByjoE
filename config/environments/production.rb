5a0907aab0f2cd22ecea21b2ecfaf0abe479e235
bignian <114035931+bignian@users.noreply.github.com>
2022-03-31 09:36:14 +0000

    fix linting error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,4 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  
