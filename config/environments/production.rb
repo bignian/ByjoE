b56d78bf7d37c9616370d25115c55808afdc140a
bignian <114035931+bignian@users.noreply.github.com>
2022-04-01 18:47:48 +0000

    add rspec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,4 +9,7 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks automatically ig
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  # Full error reports a
