74e9b23f5bb41a9625077fc31229c631f1c8a7a2
bignian <114035931+bignian@users.noreply.github.com>
2022-04-02 20:05:10 +0000

    Update TestRedShader.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       =
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Ensures that a master key
