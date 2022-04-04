796452047c0cf7892cccc89473588ef5c6e3f88d
bignian <114035931+bignian@users.noreply.github.com>
2022-04-04 14:41:40 +0000

    Added the files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in co
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+ 
