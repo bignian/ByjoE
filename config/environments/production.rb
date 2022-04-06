55d12195add6ef2b3ab4ae4420e1cd6ce0153d1f
bignian <114035931+bignian@users.noreply.github.com>
2022-04-06 13:09:36 +0000

    Revisions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/publ
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.p
