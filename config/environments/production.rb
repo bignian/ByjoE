c213693c783fba18c714dae30373c99c512a0efa
bignian <114035931+bignian@users.noreply.github.com>
2022-04-09 21:48:08 +0000

    target: Fix ->data_length re-assignment bug with SCSI overflow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS
+  # Compress CSS using a preprocessor.
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback
