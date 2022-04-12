153a6cf13c5662418058d9ba4147fb5beaea152f
bignian <114035931+bignian@users.noreply.github.com>
2022-04-12 17:43:38 +0000

    fixed if statement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,4 +33,5 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Specifies the header that your server
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # f
