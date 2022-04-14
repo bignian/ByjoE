5071001f8c5fd038229a994b3c38522d2d1cc52d
bignian <114035931+bignian@users.noreply.github.com>
2022-04-14 15:38:36 +0000

    drm/radeon: fix combios tables on older cards

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,4 +35,6 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
-  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NG
+  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
+
+  # Store uploaded files on the local file system (see config/storage
