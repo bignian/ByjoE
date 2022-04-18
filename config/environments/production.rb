e65473a35ffb8b6008325e507d2e91e5b3cd002b
bignian <114035931+bignian@users.noreply.github.com>
2022-04-18 15:39:32 +0000

    intelli_plug: use per cpu nr_runnings stats for unplugging cores

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Mount Action Cable o
+  # Mount Action Cable outside main process or domain.
+  # config.action_cable.mount_path = nil
+  # config.actio
