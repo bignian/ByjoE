6bc63130e666cd68eb55d6d3eff32ddf2b4633f8
bignian <114035931+bignian@users.noreply.github.com>
2022-04-24 10:27:13 +0000

    C6x: time: Ensure consistency in __init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_s
+  # config.force_ssl = true
+
+  # Include generic and useful information about system operation, but av
