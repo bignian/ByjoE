b10d46116688588d64e5680c7b2b132c668af1fb
bignian <114035931+bignian@users.noreply.github.com>
2022-05-09 14:19:05 +0000

    * Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Don't lo
+  # Don't log any deprecations.
+  config.active_support.report_deprecations = false
+
+  # U
