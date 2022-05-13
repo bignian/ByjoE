bed4dfd5fec156a39ffd786eaae0cb1d21792e1d
bignian <114035931+bignian@users.noreply.github.com>
2022-05-13 11:30:49 +0000

    removed index

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
-    logg
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(l
