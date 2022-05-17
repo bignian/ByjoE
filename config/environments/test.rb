7c36c7a533374fdf188c6a2a8d72fde89057a295
bignian <114035931+bignian@users.noreply.github.com>
2022-05-17 19:32:24 +0000

    Rename the kitchen config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,4 +2,5 @@ require "active_support/core_ext/integer/time"
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" f
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs. Don't rely on t
