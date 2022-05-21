1c1bdb641049a1a3996832ab9008b24adac9a4df
bignian <114035931+bignian@users.noreply.github.com>
2022-05-21 17:10:47 +0000

    Fix identation.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,4 +11,5 @@ Rails.application.configure do
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
-  # Eager lo
+  # Eager loading loads your whole application. When running a single test locally,
+  # 
