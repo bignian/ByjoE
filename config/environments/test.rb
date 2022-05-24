934954a997c0f6b3d4ec1be450f35e4f268142ea
bignian <114035931+bignian@users.noreply.github.com>
2022-05-24 23:04:08 +0000

    Add typing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,4 +22,6 @@ Rails.application.configure do
     "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error re
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_c
