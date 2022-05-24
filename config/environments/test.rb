031923d599ab5bf19f050748b8273a03ae82bb16
bignian <114035931+bignian@users.noreply.github.com>
2022-05-24 12:34:45 +0000

    nuevos cambios

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,4 +19,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    "Cach
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
+  }
+
+  # Show full error re
