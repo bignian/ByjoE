3f066622503c77deb65900aa9d7ce336f9d837b4
bignian <114035931+bignian@users.noreply.github.com>
2022-05-25 20:42:14 +0000

    Add Coveralls badge

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,4 +24,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_c
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+
+  # 
