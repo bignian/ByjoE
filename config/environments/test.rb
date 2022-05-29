f6704b64a6b43b6cea29df81c1e27aa82cd56701
bignian <114035931+bignian@users.noreply.github.com>
2022-05-29 09:22:21 +0000

    Pontoon: Update Croatian (hr) localization of AMO Frontend

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Tell Action Mailer not to d
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails i
