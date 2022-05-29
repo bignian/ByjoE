1f9909890964825397b55776de55a4945e0a11d1
bignian <114035931+bignian@users.noreply.github.com>
2022-05-29 21:04:03 +0000

    :dancer:

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails i
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.
