1389870a72d03d92313c3e3d02e34044ce6a3089
bignian <114035931+bignian@users.noreply.github.com>
2022-06-04 20:32:46 +0000

    fixed #1

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwir
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-lo
