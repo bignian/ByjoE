ac927a1bbadf99d7be8500bc5736a7f9972fbb6c
bignian <114035931+bignian@users.noreply.github.com>
2022-06-11 11:35:56 +0000

    fix initializing non-clc remote services

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,4 +2,5 @@
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
-# are not: uncommented lines 
+# are not: uncommented lines are intended to protect your configuration from
+# breaking changes in upgrades (i.e.,
