8f77347c5f2721ca6e9bd35ff9e403e07476b4a4
bignian <114035931+bignian@users.noreply.github.com>
2022-06-11 21:17:43 +0000

    add profile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,4 +3,5 @@
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
-# breaking changes in upgrades (i.e.,
+# breaking changes in upgrades (i.e., in the event that future versions of
+# Devise change the default values for those options
