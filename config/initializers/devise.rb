5d6b01b27852fa53a357e52c1578746f590090d6
bignian <114035931+bignian@users.noreply.github.com>
2022-06-13 22:33:44 +0000

    newline fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,4 +7,5 @@
 # Devise change the default values for those options).
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of 
+# Many of these configuration options can be set straight in your model.
+Devise.setup do 
