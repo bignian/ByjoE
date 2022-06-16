db8684c6cc37050e49d68c1bd8ec9d047b4476a4
bignian <114035931+bignian@users.noreply.github.com>
2022-06-16 07:36:02 +0000

    Bumped up version.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,4 +8,6 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-Devise.setup do 
+Devise.setup do |config|
+  # The secret key used by Devise. Devise uses this key to generate
+  # ran
