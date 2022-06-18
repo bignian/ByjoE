22aea5e3c99fbacc0b9f6c5aaafb9b0627e5d1f6
bignian <114035931+bignian@users.noreply.github.com>
2022-06-18 16:00:03 +0000

    add reset

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,4 +11,5 @@
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
-  # confirmation, rese
+  # confirmation, reset password and unlock tokens in the database.
+  # Devise will use the `secret_key_bas
