6df74933815ef73349070fab3a68ec61c5fcc4f8
bignian <114035931+bignian@users.noreply.github.com>
2022-06-19 20:45:02 +0000

    Regenerated gemspec for version 0.3.0

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,4 +12,6 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_bas
+  # Devise will use the `secret_key_base` as its `secret_key`
+  # by default. You can change it below and use your own secret key.
+ 
