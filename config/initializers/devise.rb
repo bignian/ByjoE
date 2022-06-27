282def4184faddf02393b4c271106e3edf4b38a8
bignian <114035931+bignian@users.noreply.github.com>
2022-06-27 06:04:35 +0000

    ws fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,4 +22,6 @@ Devise.setup do |config|
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
-  # note that it will be overwritten if you u
+  # note that it will be overwritten if you use your own mailer class
+  # with default "from" parameter.
+  config.mailer_sender = 'please-c
