aafced33e4129f6f0ee8d3adf918a1b2876e877a
bignian <114035931+bignian@users.noreply.github.com>
2022-06-29 19:47:16 +0000

    0.1.3 release

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,4 +29,5 @@ Devise.setup do |config|
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
-  # Configure th
+  # Configure the parent class responsible to send e-mails.
+  # config.parent_mailer =
