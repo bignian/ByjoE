72702faf3caa6f526386a397e2139ff9c6f86d5a
bignian <114035931+bignian@users.noreply.github.com>
2022-07-04 13:38:26 +0000

    Better icon

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -35,4 +35,7 @@ Devise.setup do |config|
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
- 
+  # available as additional gems.
+  require 'devise/orm/active_record'
+
+  # ==> Configuration for any aut
