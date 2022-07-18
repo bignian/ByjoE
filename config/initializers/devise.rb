c2b66a6fbad282ac07ab800c7a4e6bbea6c2d903
bignian <114035931+bignian@users.noreply.github.com>
2022-07-18 18:25:05 +0000

    XML

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -57,4 +57,7 @@ Devise.setup do |config|
 
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
-  # to authenticate or find a user. Default is :emai
+  # to authenticate or find a user. Default is :email.
+  config.case_insensitive_keys = [:email]
+
+  # Configure which authentication keys s
