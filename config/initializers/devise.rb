b41091cec640948f27b50dc1827b60b8fc7e95ee
bignian <114035931+bignian@users.noreply.github.com>
2022-08-17 16:23:25 +0000

    fix cors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -108,4 +108,8 @@ Devise.setup do |config|
   # When false, Devise will not attempt to reload routes on eager load.
   # This can reduce the time taken to boot the app but if your application
   # requires the Devise mappings to be loaded during boot time the application
-  # won't boot
+  # won't boot properly.
+  # config.reload_routes = true
+
+  # ==> Configuration for :database_authenticatable
+  # 
