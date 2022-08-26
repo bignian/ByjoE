6edc0a89346e8b907c9e32a2293059f95a069806
bignian <114035931+bignian@users.noreply.github.com>
2022-08-26 12:10:20 +0000

    go lint

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -129,4 +129,7 @@ Devise.setup do |config|
   # config.pepper = '00d489d9c125bda516f1c602aa4677c8fec7657184596d6a55c5cee6b4c1674a85486524b5559ae9fbc5c20a1e0c2e5d8dd3bd2f8658459ea228b647de086f3e'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notific
+  # config.send_email_changed_notification = false
+
+  # Send a notification email when the user's password is changed.
+  # confi
