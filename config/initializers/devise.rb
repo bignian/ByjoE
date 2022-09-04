965f09436fe9e1dcfa28a4575a50dcc3936733d2
bignian <114035931+bignian@users.noreply.github.com>
2022-09-04 15:04:37 +0000

    route

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -151,4 +151,6 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.
+  # config.confirm_within = 3.days
+
+  # If true, requires any email changes to be confirmed (exactly the same way as
