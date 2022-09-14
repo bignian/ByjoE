cc6dea760ca55031ac8b9444c00f20a019c82768
bignian <114035931+bignian@users.noreply.github.com>
2022-09-14 18:59:36 +0000

    pyflakes cleanup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -174,4 +174,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # con
+  # config.rememberable_options = {}
+
+  # ==> Configuration for :validatable
+  # Range f
