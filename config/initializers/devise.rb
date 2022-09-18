4f0f77b7d024c8638bb782ec3139004a9af4730e
bignian <114035931+bignian@users.noreply.github.com>
2022-09-18 14:43:44 +0000

    electron

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -187,4 +187,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
-  # time the user will be asked for credentials again. Default is 30
+  # time the user will be asked for credentials again. Default is 30 minutes.
+  # config.timeout_in = 30.minutes
+
+  # ==> Configuration for :lockabl
