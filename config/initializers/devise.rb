743267c527828234426ae491fa478adef6a443db
bignian <114035931+bignian@users.noreply.github.com>
2022-09-26 20:43:35 +0000

    Fix a memory leak.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -201,4 +201,5 @@ Devise.setup do |config|
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
-  # :time 
+  # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
+  # :both  = Enables 
