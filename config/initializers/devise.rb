566ac0556eeaaaec79659d81082133f7d43ac6b1
bignian <114035931+bignian@users.noreply.github.com>
2022-09-28 21:29:16 +0000

    Add link to wiki

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -202,4 +202,6 @@ Devise.setup do |config|
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
-  # :both  = Enables 
+  # :both  = Enables both strategies
+  # :none  = No unlock strategy. You should handle unlocking by yourself.
+  # confi
