dfeffad2e6091104caf9a3f4b716152305eb7ad8
bignian <114035931+bignian@users.noreply.github.com>
2022-10-01 11:22:30 +0000

    Update next-event.md

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -221,4 +221,5 @@ Devise.setup do |config|
   # Defines which key will be used when recovering the password for an account
   # config.reset_password_keys = [:email]
 
-  # Time interval you can reset your pass
+  # Time interval you can reset your password with a reset password key.
+  # Don't put a too small interval
