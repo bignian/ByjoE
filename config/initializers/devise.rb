69848669bbde627d557dc2be944f83750167037b
bignian <114035931+bignian@users.noreply.github.com>
2022-10-02 22:04:18 +0000

    Updating to latest ycmd

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -227,4 +227,7 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
 
   # When set to false, does not sign a user in automatically after their password is
-  # reset. Defaults to true, so a user is signed in automati
+  # reset. Defaults to true, so a user is signed in automatically after a reset.
+  # config.sign_in_after_reset_password = true
+
+  # ==> Configuration for :encryptable
