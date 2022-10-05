df530486b69fcc1a079f1ca23c688a8a6bb396bc
bignian <114035931+bignian@users.noreply.github.com>
2022-10-05 17:06:24 +0000

    Fixing an error introduced in the last commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,4 +234,5 @@ Devise.setup do |config|
   # Allow you to use another hashing or encryption algorithm besides bcrypt (default).
   # You can use :sha1, :sha512 or algorithms from others authentication tools as
   # :clearance_sha1, :authlogic_sha512 (then you should set stretches above to 20
-  # for default
+  # for default behavior) and :restful_authentication_sha1 (then you should set
+  # stretche
