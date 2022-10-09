df3ead26ab22fc738c76b1deae1017c379531659
bignian <114035931+bignian@users.noreply.github.com>
2022-10-09 17:44:29 +0000

    Update menu.html

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,4 +244,6 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.s
+  # config.scoped_views = false
+
+  # Configure the default scope given to Warden. By default it's the fi
