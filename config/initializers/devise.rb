d7f05a1d42b258df92a60ef36362dc9d371cbb91
bignian <114035931+bignian@users.noreply.github.com>
2022-10-09 19:30:35 +0000

    fix minor errors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -246,4 +246,6 @@ Devise.setup do |config|
   # are using only default views.
   # config.scoped_views = false
 
-  # Configure the default scope given to Warden. By default it's the fi
+  # Configure the default scope given to Warden. By default it's the first
+  # devise role declared in your routes (usually :user).
+  # config.default_s
