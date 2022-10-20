d5e86a852f18fea32be5fd15b0207d6f73d08cdf
bignian <114035931+bignian@users.noreply.github.com>
2022-10-20 11:20:47 +0000

    update notebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,4 +275,7 @@ Devise.setup do |config|
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
-  # change the failure app, you can configure them inside t
+  # change the failure app, you can configure them inside the config.warden block.
+  #
+  # config.warden do |manager|
+  #   manager
