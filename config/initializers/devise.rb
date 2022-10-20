2286234060694ecf171e118e441603c9758397fa
bignian <114035931+bignian@users.noreply.github.com>
2022-10-20 19:54:34 +0000

    clean up gitignore

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -278,4 +278,5 @@ Devise.setup do |config|
   # change the failure app, you can configure them inside the config.warden block.
   #
   # config.warden do |manager|
-  #   manager
+  #   manager.intercept_401 = false
+  #   manager.default_strategies(scope: :user).unshift :som
