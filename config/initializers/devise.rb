cd867a1ffaaf9088f80d5c23d325ec9953a3c4c9
bignian <114035931+bignian@users.noreply.github.com>
2022-10-21 19:24:36 +0000

    Fix keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -285,4 +285,6 @@ Devise.setup do |config|
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
-  # The followi
+  # The following options are available, assuming the engine is mounted as:
+  #
+  #     mount My
