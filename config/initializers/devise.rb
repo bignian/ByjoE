c7913c90634a51a3c96e1695f4046c162cf156e4
bignian <114035931+bignian@users.noreply.github.com>
2022-10-27 19:43:17 +0000

    style cleanup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -306,4 +306,6 @@ Devise.setup do |config|
   # ==> Configuration for :registerable
 
   # When set to false, does not sign a user in automatically after their password is
-  # changed. Defaults to true, so a user is signed in automatically after changing a pas
+  # changed. Defaults to true, so a user is signed in automatically after changing a password.
+  # config.sign_in_after_change_password = true
+end
