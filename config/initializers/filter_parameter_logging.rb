aa5258ab36b4ff313cc7ed160bfe9924b88cee16
bignian <114035931+bignian@users.noreply.github.com>
2022-10-29 16:10:43 +0000

    Addressed review comments.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Configure parameters to be filtered from the log fi
+# Configure parameters to be filtered from the log file. Use this to limit dissemination of
+# sensitive information. See the Ac
