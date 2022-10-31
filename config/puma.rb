7a60a62dcf85e576c0adeab5e8aef71f41b7afc0
bignian <114035931+bignian@users.noreply.github.com>
2022-10-31 18:46:37 +0000

    better caching

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pool
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set
