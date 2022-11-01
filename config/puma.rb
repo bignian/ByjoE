c675a6cd9d88ce853262385dfd142b42f4ad574d
bignian <114035931+bignian@users.noreply.github.com>
2022-11-01 09:08:37 +0000

    v 0.0.1

--- config/puma.rb
+++ config/puma.rb
@@ -5,4 +5,5 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads
