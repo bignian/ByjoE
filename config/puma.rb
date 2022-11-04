95af21841884314320171cd6db4b2e362924f06b
bignian <114035931+bignian@users.noreply.github.com>
2022-11-04 13:07:58 +0000

    update READY & content testing

--- config/puma.rb
+++ config/puma.rb
@@ -19,4 +19,7 @@ port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "develop
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PI
