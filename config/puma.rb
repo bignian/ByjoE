ee28a826adf4b63ae64fe61895b040164a9356df
bignian <114035931+bignian@users.noreply.github.com>
2022-11-04 21:53:37 +0000

    cfq-iosched: fix incorrect filing of rt async cfqq

--- config/puma.rb
+++ config/puma.rb
@@ -22,4 +22,6 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PI
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# Specifies the number of `workers` to boot 
