1d51f21d99c73a3d66331f66f119cc7f43587a11
bignian <114035931+bignian@users.noreply.github.com>
2022-11-06 12:31:35 +0000

    3g

--- config/puma.rb
+++ config/puma.rb
@@ -27,4 +27,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
-# Workers do n
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fe
