8a230c476177260c7d402b44c2a7d1e569bedccf
bignian <114035931+bignian@users.noreply.github.com>
2022-11-12 15:28:32 +0000

    Remove underscore dependency

--- config/puma.rb
+++ config/puma.rb
@@ -35,4 +35,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
-# process
+# process behavior so workers use less memory.
+#
+# preload_app!
+
+# Allow puma to be restarted by `bin/rail
