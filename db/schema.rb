a50543ca5eb71e7d6908b3904e83b718a08dfc85
bignian <114035931+bignian@users.noreply.github.com>
2022-11-15 06:29:19 +0000

    parport: disable PC-style parallel port support on cris

--- db/schema.rb
+++ db/schema.rb
@@ -1,1 +1,2 @@
-# This file is auto-generated from the current state of the datab
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of 
