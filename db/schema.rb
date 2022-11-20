c5ef3cc20d7eceff8fb768696cff7efd184feb4e
bignian <114035931+bignian@users.noreply.github.com>
2022-11-20 08:04:17 +0000

    add elasticsearch

--- db/schema.rb
+++ db/schema.rb
@@ -2,4 +2,5 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load`
