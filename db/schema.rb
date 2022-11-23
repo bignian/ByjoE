e5ef844868896e5bf1456fd843034209a56fb489
bignian <114035931+bignian@users.noreply.github.com>
2022-11-23 09:20:53 +0000

    Update all the things

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
-  # These are extensions that must be enabled in ord
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :ca
