d02887a4305b850286f03ba3aa3e97ec37d15763
bignian <114035931+bignian@users.noreply.github.com>
2022-11-23 08:24:18 +0000

    Cleaned up examples

--- db/schema.rb
+++ db/schema.rb
@@ -10,4 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(versi
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
+  # These are extensions that must be enabled in ord
