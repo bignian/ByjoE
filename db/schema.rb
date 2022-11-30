acec9bb25c953c7f8837417ccdaebd171178f682
bignian <114035931+bignian@users.noreply.github.com>
2022-11-30 07:58:45 +0000

    static fix

--- db/schema.rb
+++ db/schema.rb
@@ -23,4 +23,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: fal
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: fal
