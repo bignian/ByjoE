cbdbb746f42e1258ca54894a0b53bf81be9b1e83
bignian <114035931+bignian@users.noreply.github.com>
2022-12-01 18:26:50 +0000

    Update /out

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: fal
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_s
