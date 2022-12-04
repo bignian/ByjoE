0e5eb25f0c5190894d05c1a3e30f19029894f619
bignian <114035931+bignian@users.noreply.github.com>
2022-12-04 09:09:37 +0000

    cx

--- db/schema.rb
+++ db/schema.rb
@@ -26,4 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_s
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: fa
