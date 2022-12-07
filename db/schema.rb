f0b7981379d558e33ced51804bd60f2985dcb4d7
bignian <114035931+bignian@users.noreply.github.com>
2022-12-07 12:43:11 +0000

    netfilter: xt_qtaguid: extend iface stat to report protocols

--- db/schema.rb
+++ db/schema.rb
@@ -36,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_124241) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], n
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], nam
