1e7ccfc11acaf3103e4799252907c40f14e368d2
bignian <114035931+bignian@users.noreply.github.com>
2023-02-02 16:48:56 +0000

    updated README.md

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the AdminHelper. For example:
 #
-# descri
+# describe AdminHelper do
+#   describe "string concat" do
+#     it "concats two s
