b8a37a783afc055d49243c5ef06c0ebcec00fe11
bignian <114035931+bignian@users.noreply.github.com>
2023-02-11 12:53:21 +0000

    Build for production

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -66,3 +66,71 @@ RSpec.describe "/posts", type: :request do
       end
 
       it "redirects to the created post" do
+        post posts_url, params: { post: valid_attributes }
+        expect(response).to redirect_to(post_url(Post.last))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Post" do
+        expect {
+          post posts_url, params: { post: invalid_attributes }
+        }.to change(Post, :count).by(0)
+      end
+
+    
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post posts_url, params: { post: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      it "updates the requested post" do
+        post = Post.create! valid_attributes
+        patch post_url(post), params: { post: new_attributes }
+        post.reload
+        skip("Add assertions for updated state")
+      end
+
+      it "redirects to the post" do
+        post = Post.create! valid_attributes
+        patch post_url(post), params: { post: new_attributes }
+        post.reload
+        expect(response).to redirect_to(post_url(post))
+      end
+    end
+
+    context "with invalid parameters" do
+    
+      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
+        post = Post.create! valid_attributes
+        patch post_url(post), params: { post: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested post" do
+      post = Post.create! valid_attributes
+      expect {
+        delete post_url(post)
+      }.to change(Post, :count).by(-1)
+    end
+
+    it "redirects to the posts list" do
+      post = Post.create! valid_attributes
+      delete post_url(post)
+      expect(response).to redirect_to(posts_url)
+    end
+  end
+end
