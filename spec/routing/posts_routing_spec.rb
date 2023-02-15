01135016ee5d6903edfc16a22eee0c38368a4bcd
bignian <114035931+bignian@users.noreply.github.com>
2023-02-15 12:37:28 +0000

    Javadoc cleanup.

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,4 +1,6 @@
 require "rails_helper"
 
 RSpec.describe PostsController, type: :routing do
-  describe "r
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/posts").to route_to("po
