42e0834802db959404687c4bb4ec2e3ee9576e9c
bignian <114035931+bignian@users.noreply.github.com>
2023-02-16 18:23:07 +0000

    TODOS

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 RSpec.describe PostsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/posts").to route_to("po
+      expect(get: "/posts").to route_to("posts#index")
+    end
+
+    it "routes to #new" do
+      expect(get: "/posts/new").to rout
