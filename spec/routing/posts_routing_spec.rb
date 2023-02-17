5807a66fd58352fc9f36cfd22c0fd8210803e346
bignian <114035931+bignian@users.noreply.github.com>
2023-02-17 21:45:36 +0000

    video: kyro: fix incorrect sizes when copying to userspace

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe PostsController, type: :routing do
     end
 
     it "routes to #edit" do
-      expect(get: "/posts/1/edit").to route_to("posts#edit", id: 
+      expect(get: "/posts/1/edit").to route_to("posts#edit", id: "1")
+    end
+
+
+    it "routes to #create" do
+      expect(post: "/posts").to route_to("posts#create")
