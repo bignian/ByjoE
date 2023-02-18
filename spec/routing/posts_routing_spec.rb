1e53d40a9f3ea25c0fcbc028255e5776f4b574e7
bignian <114035931+bignian@users.noreply.github.com>
2023-02-18 20:30:33 +0000

    Added Korean(ko) translations

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -28,4 +28,8 @@ RSpec.describe PostsController, type: :routing do
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/po
+      expect(patch: "/posts/1").to route_to("posts#update", id: "1")
+    end
+
+    it "routes to #destroy" do
+      e
