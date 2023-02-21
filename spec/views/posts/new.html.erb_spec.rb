06121967faa5707a6dbc3f216df8c8e88300ac25
bignian <114035931+bignian@users.noreply.github.com>
2023-02-21 18:45:51 +0000

    3.4.33

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe "posts/new", type: :view do
   before(:each) do
     assign(:post, Post.new(
-    
+      name: "MyString",
+      title: "MyString",
+      content: "MyText"
+    ))
+  e
