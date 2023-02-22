85b5cd237a21d178ff6eb5ab2335c2aebf73bcce
bignian <114035931+bignian@users.noreply.github.com>
2023-02-22 14:36:54 +0000

    update loader

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe "posts/new", type: :view do
   it "renders new post form" do
     render
 
-    assert_select "form[action=?][method=?
+    assert_select "form[action=?][method=?]", posts_path, "post" do
+
+      assert_select "input[name=?]", "post[name]"
+
+      ass
