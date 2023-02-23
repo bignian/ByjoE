b173b700d848021968b07ce3fd9b56045596010a
bignian <114035931+bignian@users.noreply.github.com>
2023-02-23 22:22:19 +0000

    fix validator

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "posts/show
+RSpec.describe "posts/show", type: :view do
+  before(:each) do
+    assign(:post, Post.create!(
+      name: "Name",
+      title: "Title"
