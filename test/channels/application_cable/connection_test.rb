e7d0912eedd02547e8a072f9d9850754c9316989
bignian <114035931+bignian@users.noreply.github.com>
2023-02-27 17:13:32 +0000

    Synced with changes in framework. Use KObject->getService() instead of KFactory::get() and KObject->getIdentifier() instead of KIdentifier::identify() or $this->_identifier.

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,4 +1,9 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies"
+  # test "connects with cookies" do
+  #   cookies.signed[:user_id] = 42
+  #
+  #   connect
+  #
+  #   assert_equal connection
